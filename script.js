574d5435b4a832be8cd063a5f51c236853c7339a
louisgtavo21 <louisgustavo07@gmail.com>
2021-08-23 00:09:13 +0700

    Edit files

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-console.log('Load js')
+// Mobile navbar
+const nav = document.getElementById('Navbar'),
+    btn = document.getElementById("btn-mobile"),
+    menu = document.getElementById("mobile-menu"),
+    content = document.getElementById("mobile-menu-content"),
+    expand = document.getElementById("icon-expand"),
+    collapse = document.getElementById("icon-collapse")
 
-const nav = document.getElementById('Navbar');
-
-// window.onscroll = function(){
-//     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-//         nav.classList.add("nav-colored");
-//         nav.classList.remove("nav-transparent");
-//     } 
-//     else {
-//         nav.classList.remove("nav-colored");
-//         nav.classList.add("nav-transparent");
-//     }
-// }
-
-const btn = document.getElementById("btn-mobile")
-const menu = document.getElementById("mobile-menu")
-const content = document.getElementById("mobile-menu-content")
-const expand = document.getElementById("icon-expand")
-const collapse = document.getElementById("icon-collapse")
+const hamburger = document.getElementById("hamburger")
 
 // add event listeners
 btn.addEventListener("click", () => {
@@ -25,10 +14,75 @@ btn.addEventListener("click", () => {
     // if (nav.classList.contains("nav-transparent")){
     //     nav.classList.toggle("nav-colored-2")
     // }
-
-    nav.classList.toggle("nav-colored-2")
+    hamburger.classList.toggle('expand')
     menu.classList.toggle("opacity-100");
     content.classList.toggle("hidden");
-    expand.classList.toggle("hidden");
-    collapse.classList.toggle("hidden");
+    // expand.classList.toggle("hidden");
+    // collapse.classList.toggle("hidden");
 });
+
+// Typing effect
+var word = 0,
+    text = 'LOUISITE._GUSTAVO.',
+    speed = 200,
+    heading1 = document.getElementById('first-name'),
+    flag = 0,
+    flag2 = 0;
+    
+window.onload = function typing(){
+    if (word < text.length){
+        if (flag == 1){
+            if (text.charAt(word) === 'S'){
+                flag--
+                word+=5
+            }
+            else {
+                var currentH1 = heading1.innerText;
+
+                heading1.innerText = currentH1.substr(0, currentH1.length - 1)
+                word--
+            }
+        }
+        else if (text.charAt(word) === '_'){
+            heading1 = document.getElementById('last-name')
+            word++
+        }
+        else if (text.charAt(word) === '.' && text.charAt(word+1) === '_'){
+            heading1.innerText += text.charAt(word)
+            flag++
+	    flag2++
+        }
+        else {
+            heading1.innerText += text.charAt(word)
+            word++
+        }
+
+        if (flag2){
+	    setTimeout(typing, 1000)
+	    flag2--
+	}
+        else
+            setTimeout(typing, speed)
+    }
+    else{
+    	document.getElementById('vertical-bar').classList.add('animate-blink')
+    }
+}
+
+// setInterval(function(){
+//     console.log("Refreshing window.innerWidth: " + window.innerWidth)
+//     var from = document.getElementsByClassName("exp-desc")
+//     var to = document.getElementsByClassName("exp-desc-wrapper")
+//     var tom = document.getElementsByClassName("exp-desc-mobile-wrapper")
+
+//     if (window.innerWidth < 640){
+//         for (let i = 0; i < from.length; i++){ 
+//             tom[i].appendChild(from[i])
+//         }
+//     }
+//     else{
+//         for (let i = 0; i < tom.length; i++){ 
+//             to[i].appendChild(from[i])
+//         }
+//     }
+// }, 100)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Nunito Sans', 'sans-serif']
-      }
-    },
     screens: {
+      '2xs': '360px',
+      'xs2': '414px',
       'xs': '480px',
       sm: '640px',
       md: '768px',
@@ -17,9 +14,39 @@ module.exports = {
       xl: '1280px',
       '2xl': '1536px',
     },
+    extend: {
+      fontFamily: {
+        sans: ['Nunito Sans', 'sans-serif']
+      },
+      colors: {
+        'gray-1000': 'rgb(6, 16, 29)',
+      },
+      spacing: {
+        '15': '3.75rem',
+        '1/10': '10%',
+        '3/20': '15%',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%' : { 
+            transform: 'translateY(-15%)'
+          },
+        },
+        pulse: {
+          '50%': { 
+            opacity: '.75' 
+          },
+        },
+      },
+      width: {
+        '1/8': '12.5555555%',
+        '7/8': '87.5555555%',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    }
   },
   plugins: [],
 }
